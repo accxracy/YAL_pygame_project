b5f38efad446c0fdac4fc82318037a82cb896702
Вячеслав <@>
2025-01-02 20:58:44 +0200

    added initialization updated readme

---
+++ initialization.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+def init():
+    pygame.init()
+    pygame.display.set_caption('Верю-не-верю')
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+
+    running = True
+
+    fps = 100
+    clock = pygame.time.Clock()
+
+    while running:
+        screen.fill(pygame.Color('black'))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import pygame
+from initialization import init
+
+
+if __name__ == '__main__':
+    init()
