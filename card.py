4b7e77c610a06e217b48f2020d5240095de12a24
Вячеслав <@>
2025-01-03 17:17:57 +0200

    Класс card и функция load_deck

--- card.py
+++ card.py
@@ -1,2 +1,36 @@
+import pygame, os, sys
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 class Card:
-    pass
+    def __init__(self, set_number, suit, rank):
+        self.card = load_image(f'cards/cards_set_{set_number}/{suit}_{rank}.png')
+
+    def get_card(self):
+        return self.card
+
+
+def load_deck():
+    deck = []
+    for i in range(1, 3):
+        for j in range(4):
+            for k in range(2, 15):
+                deck.append(Card(i, j, k).get_card())
+    return deck
