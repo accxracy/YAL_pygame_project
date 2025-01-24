418bd285b5b2808c6b385a1f8c7459e5d192cb3b
accxracy <gkola22907@gmail.com>
2025-01-24 20:13:13 +0200

    rebased game_chooser + settings menus

--- initialization.py
+++ initialization.py
@@ -1,9 +1,10 @@
 import pygame, sys, os
 from main_menu_buttons import Button
-from GOOOOL import football_game
-from Nauru import nauru_game
 from cursor import sprite, all_sprites
 from authors import authors
+from settings import settings_menu
+from game_chooser import game_chooser
+
 
 pygame.init()
 WIDTH, HEIGHT = 1280, 720
@@ -81,10 +82,10 @@ def main_menu():
                 sprite.rect.x, sprite.rect.y = coords
 
             if event.type == pygame.USEREVENT and event.button == play_button:
-                game()
+                game_chooser(SCREEN)
 
             if event.type == pygame.USEREVENT and event.button == settings_button:
-                settings_menu()
+                settings_menu(SCREEN)
 
             if event.type == pygame.USEREVENT and event.button == final_button:
                 authors(SCREEN)
@@ -106,154 +107,6 @@ def main_menu():
         if flag:
             all_sprites.draw(SCREEN)
 
-        pygame.display.flip()
-
-
-def settings_menu():
-    flag = True
-
-    with open('data/settings/settings.ini', 'r+') as fin:
-        settings = fin.read()
-        print(settings)
-    global deck_number
-    deck_number = settings.split('deck_type=')[1]
-
-
-    back_button = Button((600, 600), 250, 100, "Назад", font,
-                         "data/buttons/quit_button.png",
-                         "data/buttons/quit_button_hover.png",
-                         "data/sounds/click.wav")
-    deck1_button = Button((10, 300), 200, 200, None, font,
-                          "data/buttons/full_1.png",
-                          "data/buttons/full_1_hover.png",
-                          "data/sounds/click.wav")
-    deck2_button = Button((300, 300), 200, 200, None, font,
-                          "data/buttons/full_2.png",
-                          "data/buttons/full_2_hover.png",
-                          "data/sounds/click.wav")
-    apply_button = Button((300, 600), 250, 100, 'Применить', font,
-                          "data/buttons/option_button.png",
-                          "data/buttons/option_button_hover.png",
-                          "data/sounds/click.wav")
-    running = True
-    while running:
-        SCREEN.fill((0, 0, 0))
-        SCREEN.blit(BG_menu, (0, 0))
-
-        text_surface = font.render("Настройки", True, (255, 255, 255))
-        text_rect = text_surface.get_rect(center=(WIDTH / 2, 30))
-        SCREEN.blit(text_surface, text_rect)
-        text_surface_type = font.render(f"тип колоды: {deck_number}", True, (255, 255, 255))
-        text_rect_type = text_surface_type.get_rect(center=(200, 100))
-        SCREEN.blit(text_surface_type, text_rect_type)
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.MOUSEMOTION:
-                coords = event.pos
-                flag = pygame.mouse.get_focused()
-                sprite.rect.x, sprite.rect.y = coords
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-            if event.type == pygame.USEREVENT and event.button == back_button:
-                running = False
-
-            if event.type == pygame.USEREVENT and event.button == deck1_button:
-                deck_number = 1
-            if event.type == pygame.USEREVENT and event.button == deck2_button:
-                deck_number = 2
-
-            if event.type == pygame.USEREVENT and event.button == apply_button:
-                with open('data/settings/settings.ini', 'w') as fout:
-                    fout.write(f'deck_type={deck_number}')
-
-            for btn in [back_button, deck1_button, deck2_button, apply_button]:
-                btn.han_event(event)
-        for btn in [back_button, deck1_button, deck2_button, apply_button]:
-            btn.checking_hover(pygame.mouse.get_pos())
-            btn.draw(SCREEN)
-
-        if flag:
-            all_sprites.draw(SCREEN)
-        pygame.display.flip()
-
-
-def game():
-
-    flag = True
-    back_button = Button((50, 50), 150, 75, "Назад", pygame.font.Font('data/fonts/Verdana.ttf', 20),
-                         "data/buttons/quit_button.png",
-                         "data/buttons/quit_button_hover.png",
-                         "data/sounds/click.wav")
-
-    nauru_button = Button((200, 300), 250, 100, "Курочка", pygame.font.Font('data/fonts/Verdana.ttf', 20),
-                         "data/buttons/play_button.png",
-                         "data/buttons/play_button_hover.png",
-                         "data/sounds/click.wav")
-    football_button = Button((800, 300), 250, 100, "Футбольчик", pygame.font.Font('data/fonts/Verdana.ttf', 20),
-                          "data/buttons/play_button.png",
-                          "data/buttons/play_button_hover.png",
-                          "data/sounds/click.wav")
-
-    running = True
-
-
-    while running:
-        SCREEN.fill((0, 0, 0))
-        SCREEN.blit(BG_game, (0, 0))
-
-        rules_nauru = font.render("Выберите игру", True, (255, 255, 255))
-        text_rect = rules_nauru.get_rect(center=(WIDTH / 2, 30))
-        SCREEN.blit(rules_nauru, text_rect)
-
-        rules_nauru = font.render("ПРАВИЛА КУРОЧКИ", True, (255, 255, 255))
-        text_rect = rules_nauru.get_rect(center=(300, 500))
-        SCREEN.blit(rules_nauru, text_rect)
-        rules_football = font.render(f"ПРАВИЛА ФУТБОЛЬЧИКА", True, (255, 255, 255))
-        text_rect_type = rules_football.get_rect(center=(900, 500))
-        SCREEN.blit(rules_football, text_rect_type)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.MOUSEMOTION:
-                coords = event.pos
-                flag = pygame.mouse.get_focused()
-                sprite.rect.x, sprite.rect.y = coords
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-
-            if event.type == pygame.USEREVENT and event.button == back_button:
-                running = False
-
-            if event.type == pygame.USEREVENT and event.button == nauru_button:
-                nauru_game(SCREEN)
-
-            if event.type == pygame.USEREVENT and event.button == football_button:
-                football_game(SCREEN)
-
-            for btn in [back_button, football_button, nauru_button]:
-                btn.han_event(event)
-        for btn in [back_button, football_button, nauru_button]:
-            btn.checking_hover(pygame.mouse.get_pos())
-            btn.draw(SCREEN)
-
-        if flag:
-            all_sprites.draw(SCREEN)
         pygame.display.flip()
 
 
