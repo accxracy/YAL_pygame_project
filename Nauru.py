04bfe9de5747ac1c3e305150ff05fe019f439e7e
Вячеслав <@>
2025-01-24 18:47:42 +0200

    Смена курса

---
+++ Nauru.py
@@ -0,0 +1,34 @@
+import pygame, sys, os
+from main_menu_buttons import Button
+from card import load_deck, Card, create_deck
+
+pygame.init()
+WIDTH, HEIGHT = 1280, 720
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Карточные игры)")
+deck_number = 1
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+all_sprites = pygame.sprite.Group()
+sprite = pygame.sprite.Sprite()
+sprite.image = load_image("arrow.png")
+sprite.rect = sprite.image.get_rect()
+all_sprites.add(sprite)
+pygame.mouse.set_visible(False)
